746c0fd9874d66cb92f3886f0442f8f533e5db94
hubert <jessahubert@gmail.com>
2025-11-19 17:38:39 +0100

    ss

--- src/main/java/Part8/VehicleRegistry.java
+++ src/main/java/Part8/VehicleRegistry.java
@@ -11,8 +11,8 @@ public class VehicleRegistry {
     }
 
     public boolean add(LicensePlate licensePlate, String owner){
-        if(vR.get(licensePlate) == null){
-            vR.replace(licensePlate,owner);
+        if(!vR.containsKey(licensePlate)){
+            vR.put(licensePlate, owner);
             return true;
         }
         return false;
@@ -22,9 +22,27 @@ public class VehicleRegistry {
         if(vR.get(licensePlate) != null){
             return vR.get(licensePlate);
         }
+
+        return null;
     }
 
     public boolean remove(LicensePlate licensePlate){
+        if (vR.containsKey(licensePlate)) {
+            vR.remove(licensePlate);
+            return true;
+        }
+        return false;
+    }
 
+    public void printLicensePlates(){
+        for (LicensePlate licensePlate : vR.keySet()) {
+            System.out.println(licensePlate);
+        }
+    }
+
+    public void printOwners(){
+        for (String value : vR.values()) {
+            System.out.println(value);
+        }
     }
 }
