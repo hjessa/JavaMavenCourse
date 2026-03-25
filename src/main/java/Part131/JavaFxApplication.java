8ad905d7178b9b34e34291b4cc386d6cc2a42d6c
hubert <jessahubert@gmail.com>
2026-03-25 18:08:30 +0100

    ss

---
+++ src/main/java/Part131/JavaFxApplication.java
@@ -0,0 +1,28 @@
+package Part131;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class JavaFxApplication extends Application {
+
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("This is a button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+
+        Scene scene = new Scene(componentGroup);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(JavaFxApplication.class);
+    }
+}
