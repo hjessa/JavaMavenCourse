c4f0016ad71ba0ce3d3cf3677e5d63c6e547533c
hubert <jessahubert@gmail.com>
2026-04-15 20:15:16 +0200

    ss

--- src/main/java/Part141/PartiesApplication.java
+++ src/main/java/Part141/PartiesApplication.java
@@ -7,12 +7,22 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Math.min;
+
 public class PartiesApplication extends Application {
 
     public void start(Stage stage){
 
-        NumberAxis xAxis = new NumberAxis(1000,2000,2);
-        NumberAxis yAxis = new NumberAxis(0,100,5);
+        NumberAxis xAxis = new NumberAxis(1968,2007,4);
+        NumberAxis yAxis = new NumberAxis(0,30,5);
 
         yAxis.setLabel("Value");
         xAxis.setLabel("Years");
@@ -34,6 +44,32 @@ public class PartiesApplication extends Application {
     public void readWriteData(String filename){
 
         //Odczytam dane z pliku
+        List<XYChart.Series<Number,Number>> seriesList = new ArrayList<>();
+
+        try(Scanner file = new Scanner(Paths.get(filename))){
+            //muszę pominąć 1 wiersz
+            String years = file.nextLine();
+            String[] splittedYears = years.split("\t");
+            while(file.hasNextLine()){
+
+                String line = file.nextLine();
+                String[] row = line.split("\t");
+
+                XYChart.Series<Number,Number> series = new XYChart.Series<>();
+
+                //Ustawiam 1 jako nazwe
+                series.setName(row[0]);
+                for (int i = 1; i < min(row.length, splittedYears.length); i++) {
+                    series.getData().add(new XYChart.Data<>(Integer.valueOf(splittedYears[i]),Double.valueOf(row[i])));
+                }
+
+                seriesList.add(series);
+
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         //Pierwsza linia mi nada nazwy serii
         //Kolejna będzie wstawiać do serii
         //na końcu dodam do linechartu wszystkie serie
